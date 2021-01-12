1e8d532cdb1700fd5d98c0f39a8a9408faec3e54
RodrigoGemaque <ro.gemaque16@gmail.com>
2021-01-12 17:05:39 -0300

    lesson 173- helper for take I18n

--- app/helpers/admins_backoffice_helper.rb
+++ app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,10 @@
 module AdminsBackofficeHelper
+  
+  def translate_attribute(object = nil, method = nil)
+    if object && method
+      object.model.human_attribute_name(method)
+    else
+      puts "Informe os parametros corretamente"
+    end
+  end
 end
