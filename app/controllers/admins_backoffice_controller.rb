cf66594ed7db512a00084be4ee3ff38d5442b193
RodrigoGemaque <ro.gemaque16@gmail.com>
2021-01-14 17:58:20 -0300

    lesson - 184- Fix lose layout admins backoffice

--- app/controllers/admins_backoffice_controller.rb
+++ app/controllers/admins_backoffice_controller.rb
@@ -2,5 +2,5 @@ class AdminsBackofficeController < ApplicationController
   before_action :authenticate_admin!
   # skip_before_action :verify_authenticity_token, :only => :create
   # skip_before_action :verify_authenticity_token, :only => :destroy
-  
+  layout 'admins_backoffice'
 end
