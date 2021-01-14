2a4c0b76cb8a9fd93a213ed5b1df972d067b0c37
RodrigoGemaque <ro.gemaque16@gmail.com>
2021-01-14 15:24:51 -0300

    lesson - 181- devise layout seted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token, :only => :create
   skip_before_action :verify_authenticity_token, :only => :destroy
+
+  layout :layout_by_resource
+
+  private
+  def layout_by_resource
+    if devise_controller? && resource_class == Admin
+      "admin_devise"
+    else
+      "application"
+    end
+  end
 end
