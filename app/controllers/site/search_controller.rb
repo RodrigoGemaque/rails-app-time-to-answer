440a9073f6c5bb52216445b7c22f11f6fda5707f
RodrigoGemaque <ro.gemaque16@gmail.com>
2021-01-18 12:11:57 -0300

    lesson-195- search by last item added in table

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,8 +1,6 @@
 class Site::SearchController < SiteController
   def questions
     # @questions = Question.includes(:answers).page params[:page]
-    @questions = Question.includes(:answers)
-        .where("lower(description) LIKE ?", "%#{params[:term].downcase}%")
-        .page(params[:page])
+    @questions = Question.search(params[:term], params[:page])
   end
 end
