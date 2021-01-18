4acb46c9456c75f6363cf49fc3e6b28dd1a908b7
RodrigoGemaque <ro.gemaque16@gmail.com>
2021-01-18 12:13:37 -0300

    lesson-195- class methods for search

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,14 @@ class Question < ApplicationRecord
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
   max_paginates_per 5
+
+  def self.search(term,page) 
+    Question.includes(:answers)
+            .where("lower(description) LIKE ?", "%#{term.downcase}%")
+            .page(page)
+  end
+  def self.last_question(page)
+    Question.includes(:answers).order('created_at desc').page(page)
+  end
+
 end
