460853e2178fee1471eadd675bb77fba03549780
RodrigoGemaque <ro.gemaque16@gmail.com>
2021-01-19 11:00:31 -0300

    lesson-198..204 - answer routes added

--- config/routes.rb
+++ config/routes.rb
@@ -14,13 +14,12 @@ Rails.application.routes.draw do
   namespace :site do
     get 'welcome/index'
     get 'search', to: 'search#questions'
+    get 'subject/:subject_id/:subject', to: 'search#subject', as: 'search_subject'
+
+    post 'answer', to: 'answer#question'  
   end
 
   devise_for :users
   devise_for :admins
-root to: 'site/welcome#index'
+  root to: 'site/welcome#index'
 end
-
-
-
-     
